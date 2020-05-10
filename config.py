07e97a653dc079cbf196c8336646edc3627d076e
Frozen Feelings <kiprutohcollo@gmail.com>
2020-05-10 20:07:31 +0300

    created config.py

---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+
+class Config:
+    '''
+    general configuration parent class
+    '''
+    SECRET_KEY= os.environ.get('SECRET_KEY')
+    QUOTES_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://collins:qwertyui@localhost/bloggers'
+
+class ProdConfig(Config):
+    '''
+    Pruduction configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG=True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
